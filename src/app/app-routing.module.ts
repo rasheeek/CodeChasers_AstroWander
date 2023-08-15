b07e30384b3fd0ab2baf4b23044b4ac74cfb3024
Vathan Silva <vathansilva@gmail.com>
2023-08-16 02:32:28 +0530

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,13 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
+
+
 ];
 @NgModule({
   imports: [
