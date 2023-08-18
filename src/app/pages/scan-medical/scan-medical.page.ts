1aebba8826f3f7121bd548ebc40a44d200bd8dce
aasimmohamed <96591108+aasimmohamed@users.noreply.github.com>
2023-08-18 18:14:03 +0530

    latest

--- src/app/pages/scan-medical/scan-medical.page.ts
+++ src/app/pages/scan-medical/scan-medical.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-scan-medical',
@@ -11,14 +12,28 @@ export class ScanMedicalPage implements OnInit {
 
   constructor(
     private router: Router,
-    private modalCtrl : ModalController
+    private modalCtrl : ModalController,
+    private platform : Platform
   ) { }
 
   ngOnInit() {
-
-    //Mohamed and Riznas scanning codes 
-
-
+    this.platform.ready().then(async (res) => {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        await BarcodeScanner.checkPermission({ force: true });
+        BarcodeScanner.hideBackground();
+        document.querySelector('body')?.classList.add('scanner-active');
+        const result = await BarcodeScanner.startScan();
+        if (result.hasContent) {
+          BarcodeScanner.showBackground();
+          document.querySelector('body')?.classList.remove('scanner-active');
+          BarcodeScanner.stopScan();
+          this.modalCtrl.dismiss(result.content);
+        }
+      }
+      console.log('Permission rejected');
+      return false;
+    });
 
     //after value
     //  this.modalCtrl.dismiss(scan.result);
