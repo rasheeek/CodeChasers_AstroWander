57f1d86055e4a19f9f7482c97a902cbdb6fa5a18
Rasheek Mohamed <shrasheek@gmail.com>
2023-08-20 01:27:51 +0530

    Date changes

--- src/app/pages/search-crafts/search-crafts.page.ts
+++ src/app/pages/search-crafts/search-crafts.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { utcToZonedTime } from 'date-fns-tz';
 import { BookingService } from 'src/app/shared/services/booking.service';
 import { IPlanetShip } from 'src/app/shared/types/type.model';
 
@@ -10,57 +11,96 @@ import { IPlanetShip } from 'src/app/shared/types/type.model';
   styleUrls: ['./search-crafts.page.scss'],
 })
 export class SearchCraftsPage implements OnInit {
-
   selectedSortIndex = 0;
   fromPlanetFilter: string = '';
   toPlanetFilter: string = '';
   startDate: string = '';
-  occasionStartDate: string = '';
-  occasionEndDate: string = '';
+  occasionStartDate: any;
+  occasionEndDate: any;
+
+  startDateVal: string = '';
+  endDateVal: string = '';
 
   planetShips: IPlanetShip[] = [];
 
   cards = [
-    { companyName: 'CosmicJet Airlines', flightCode: 'CJ-2024A712', price: '2800k', way: 'Two way round' },
-    { companyName: 'NebulaAir Voyages', flightCode: 'NV-3056B934', price: '3100k', way: 'Two way round' },
-    { companyName: 'StarHopper Excursion', flightCode: 'SH-4028C587', price: '4900k', way: 'Two way round' },
-    { companyName: 'AstroBreeze Airlines', flightCode: 'AB-7034F109', price: '5100k', way: 'Two way round' },
+    {
+      companyName: 'CosmicJet Airlines',
+      flightCode: 'CJ-2024A712',
+      price: '2800k',
+      way: 'Two way round',
+    },
+    {
+      companyName: 'NebulaAir Voyages',
+      flightCode: 'NV-3056B934',
+      price: '3100k',
+      way: 'Two way round',
+    },
+    {
+      companyName: 'StarHopper Excursion',
+      flightCode: 'SH-4028C587',
+      price: '4900k',
+      way: 'Two way round',
+    },
+    {
+      companyName: 'AstroBreeze Airlines',
+      flightCode: 'AB-7034F109',
+      price: '5100k',
+      way: 'Two way round',
+    },
   ];
 
   constructor(
     private router: Router,
     private bookingService: BookingService,
     private loadingCtrl: LoadingController
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.loadingCtrl.create().then(loadingEl => {
+    this.loadingCtrl.create().then((loadingEl) => {
       loadingEl.present();
-      this.bookingService.getAllShips().subscribe(res => {
-        console.log("All ships", res);
-        this.planetShips = res;
-        loadingEl.dismiss();
-      }, (err => {
-        loadingEl.dismiss();
-      }))
-    })
+      this.bookingService.getAllShips().subscribe(
+        (res) => {
+          console.log('All ships', res);
+          this.planetShips = res;
+          loadingEl.dismiss();
+        },
+        (err) => {
+          loadingEl.dismiss();
+        }
+      );
+    });
   }
 
   book(selectedShip: IPlanetShip) {
     this.bookingService.selectedShip = selectedShip;
-    this.router.navigate(['/selected-flight'])
+    this.router.navigate(['/selected-flight']);
   }
 
   setStartDate(dateValue: any) {
-    // this.occasionStartDate = dateValue; 
-    const selectedDate = new Date(dateValue);
-    this.occasionStartDate = selectedDate.toISOString().split('T')[0];
+    console.log('bvalue', dateValue);
+    dateValue = dateValue.split('T')[0];
+    const dt = new Date(dateValue);
+    const selectedDate = new Date(
+      dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000
+    );
+    this.startDateVal = this.getDateStringWithoutTimeZone(selectedDate);
+  }
+
+  getDateStringWithoutTimeZone(date: Date) {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 
   setEndDate(dateValue: any) {
-    // this.occasionEndDate = dateValue; 
-    const selectedDate = new Date(dateValue);
-    this.occasionEndDate = selectedDate.toISOString().split('T')[0];
+    // this.occasionEndDate = dateValue;
+    dateValue = dateValue.split('T')[0];
+    const dt = new Date(dateValue);
+    const selectedDate = new Date(
+      dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000
+    );
+    this.endDateVal = this.getDateStringWithoutTimeZone(selectedDate);
   }
-
 }
--- src/app/shared/pipes/date-range-filter.pipe.ts
+++ src/app/shared/pipes/date-range-filter.pipe.ts
@@ -2,23 +2,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { IPlanetShip } from '../types/type.model';
 
 @Pipe({
-  name: 'dateRangeFilter'
+  name: 'dateRangeFilter',
 })
 export class DateRangeFilterPipe implements PipeTransform {
-  transform(items: IPlanetShip[], startDate: string, endDate: string): IPlanetShip[] {
+  transform(
+    items: IPlanetShip[],
+    startDate: string,
+    endDate: string
+  ): IPlanetShip[] {
     if (!startDate || !endDate) {
       return items;
     }
-    
-    const filteredItems = items.filter(item => {
-      const departureDate = new Date(item.departureDate.toDate());
-      const arrivalDate = new Date(item.arrivalDate.toDate());
-      const startRange = new Date(startDate);
-      const endRange = new Date(endDate);
 
-      return departureDate >= startRange && departureDate <= endRange &&
-             arrivalDate >= startRange && arrivalDate <= endRange;
+    const filteredItems = items.filter((item) => {
+      const departureDate = item.departureDate.toDate().toISOString().split('T')[0];
+      const arrivalDate =item.arrivalDate.toDate().toISOString().split('T')[0];
+      const startRange = startDate.split('T')[0];
+      const endRange = endDate.split('T')[0];
+      console.log("Ship dates", item.departureDate.toDate().toISOString().split('T')[0],item.arrivalDate.toDate().toISOString().split('T')[0] );
+      
+      return (
+        departureDate== startRange &&
+        arrivalDate == endRange
+      );
     });
+
+    console.log('Filtered items', filteredItems, startDate.split('T')[0], endDate.split('T')[0], items);
 
     return filteredItems;
   }
