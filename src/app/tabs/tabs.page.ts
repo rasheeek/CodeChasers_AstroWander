7dd8f305b23e93d06df0b05bd85177a46a5b2718
shaastar <mshamailhsiraj@gmail.com>
2023-08-20 18:43:26 +0530

    tab center btn and tab designs

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  isCenterBtnSelected = false
+  constructor(
+    private router: Router,
+  ) {
+    // console.log('this.router.url', this.router.url);
+    router.events.subscribe((val) => {
+      // see also 
+      // console.log('this.router.url', this.router.url);
+
+      if(this.router.url == '/tabs/home'){
+        this.isCenterBtnSelected = true;
+      }
+      else{
+        this.isCenterBtnSelected = false;
+      }
+  });
+  }
+  ngOnInit() {
+    console.log('this.router.url', this.router.url);
+  }
 
 }
